37f3e884ed47e1a4508de228b732aa9629af8263
Megan <111624182+meganhacha@users.noreply.github.com>
2024-10-30 00:16:16 -0400

    finish assignment & submit

--- Home Assignment 2/HW2_MeganHacha.cpp
+++ Home Assignment 2/HW2_MeganHacha.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-//Doing it as a separate function so I can get more practice w/pointers passed as arguments.
+//Separate function not necessary, but it's good practice.
 void calcVals(int* arr, int n) {
 
     //Just need totalHours and maxHours for now -- average can be calculated later.
@@ -23,13 +23,13 @@ void calcVals(int* arr, int n) {
           maxHours = *arr;
       };
       
-      //Move to next array element, decrement count since I'm "exhausting" the number of movements done.
+      //Move to next array element, decrement count since I'm "exhausting" the number of movements done. Another valid option is doing a for loop that increments to n - 1.
       arr++;
       count--;
       
     };
     
-    //Average is total / num of elements. It looks like some extra steps might be needed for more exact values.
+    //Average is total / num of elements. It looks like some extra steps might be needed for more exact values since entries is an array of integers.
     double avgHours = totalHours / n;
     
     //Print totalHours, avgHours, maxHours.
@@ -57,15 +57,15 @@ int main() {
     int *ptr = entries;
     
     //Pass to calcVals to do the rest of the process.
-    calcVals(entries, entrySize);
+    calcVals(ptr, entrySize);
     
 }
 /*END PROBLEM 1------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
+
 /*START PROBLEM 2----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
@@ -81,7 +81,7 @@ int main() {
     // int *ptr1 = a can't be done as this pointer isn't constant.
     
     //However, this is considered a pointer to a constant, so this one DOES work.
-    //This pointer can't edit the value at the address it's pointing to, but it is still capable of changing addresses.
+    //A constant pointer that points to a constant location is read-only, which means its address or the value of the address it's pointing to can't be changed via something like '*ptr2 = &b'.
     int const *ptr2 = &a;
     
     //This ptr acts as a regular pointer, no constant addresses pointed to, and it's not constant itself.
@@ -98,27 +98,74 @@ int main() {
     
     *ptr3 += 5;
     //The value of b is now 15.
+   
     
     cout << "Result:\n";
     cout << *ptr2 << endl;
     cout << *ptr3 << endl;
     *ptr4 += 10;
     cout << *ptr4 << endl;
-    
-/*
-    This shows that the only 
-*/
+
+    //The results show the two pointer types that can have their values changed: ptr3 and ptr4.
 
     return 0;
 }
-
-
 /*END PROBLEM 2------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
+
 /*START PROBLEM 3----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+#include <iostream>
+#include <string>
+using namespace std;
 
-
-
+int main() {
+    
+    //Pointer for dynamic allocation of students array.
+    string *students = nullptr;
+    string topStudent;
+    
+    //Pointer for dynamic allocation of grades array.
+    double *grades = nullptr;
+    double highestGrade = 0;
+    int numStudents;
+    
+    cout << "Enter the number of students:\n";
+    //Used to denote size of dynamic arrays.
+    cin >> numStudents;
+    
+    //Dynamic allocation of both students and grades arrays.
+    students = new string[numStudents];
+    grades = new double[numStudents];
+    
+    for (int i = 0; i < numStudents; i++) {
+    //Change student number in list with each iteration. Since arrays start w/0, add 1 to make it more user-friendly. (start @ student 1, not student 0).
+        cout << "Please enter the name for student " << (i+1) << ": ";
+        cin >> students[i];
+        
+        cout << "Please enter the test score for " << students[i] << ": ";
+        cin >> grades[i];
+        cout << "\n";
+    };
+    
+    //To find the top score, iterate through the grades array, find index of highest score, use that same index to find name from names array.
+    for(int i = 0; i < numStudents; i++) {
+        
+        if (grades[i] > highestGrade) {
+            highestGrade = grades[i];
+            topStudent = students[i];
+        };
+        
+    };
+    
+    cout << "The student who received the highest grade is " << topStudent << ", with a score of " << highestGrade << ".";
+    
+    //Free the allocated memory.
+    delete [] students;
+    delete [] grades;
+    students = nullptr;
+    grades = nullptr;
+    return 0;
+}
 /*END PROBLEM 3----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
